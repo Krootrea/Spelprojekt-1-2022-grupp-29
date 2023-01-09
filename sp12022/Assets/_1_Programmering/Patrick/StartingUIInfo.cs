fde4a6a06b210943be035f94b49b2380f02a1688
Patrick J <d22patjo@student.his.se>
2023-01-09 20:04:32 +0100

    StartingUI pratbubblor och min scen

--- sp12022/Assets/_1_Programmering/Patrick/StartingUIInfo.cs
+++ sp12022/Assets/_1_Programmering/Patrick/StartingUIInfo.cs
@@ -1,16 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StartingUIInfo : MonoBehaviour
 {
-    private GameObject move, jump;
+    private GameObject move, jump, friend, currentTalkBubble;
+    private Queue<GameObject> talkBubbles;
     private bool initialMoveDone, initialJumpDone;
     private void Awake(){
+        currentTalkBubble = null;
         move = transform.GetChild(0).gameObject;
         jump = transform.GetChild(1).gameObject;
+        friend = transform.Find("Friend").gameObject;
         move.SetActive(true);
+        talkBubbles = new Queue<GameObject>();
+        FillTalkBubbles();
+        HandleTalk();
+    }
+
+    private void FillTalkBubbles(){
+        foreach (RectTransform bubble in friend.transform)
+            talkBubbles.Enqueue(bubble.gameObject);
     }
 
     private void Update(){
@@ -23,6 +35,8 @@ public class StartingUIInfo : MonoBehaviour
                 initialMoveDone = true;
             }
         }
+        if ((Input.GetKeyUp(KeyCode.E) || Input.GetButtonUp("Interact")))
+            HandleTalk();
 
         if (initialMoveDone && !initialJumpDone) {
             if (Input.GetButtonDown("Jump")) {
@@ -31,7 +45,22 @@ public class StartingUIInfo : MonoBehaviour
             }
         }
 
-        if (initialJumpDone && initialMoveDone) 
+        if (initialJumpDone && initialMoveDone && !friend.activeSelf) 
             Destroy(this);
+    }
+
+    private void HandleTalk(){
+        if (!currentTalkBubble.IsUnityNull())
+        {
+               currentTalkBubble.gameObject.SetActive(false);
+               currentTalkBubble = null;
+        }
+        if (talkBubbles.Count>0)
+        {
+            currentTalkBubble = talkBubbles.Dequeue();
+            currentTalkBubble.SetActive(true);
+        }
+        else
+            friend.SetActive(false);
     }
 }
